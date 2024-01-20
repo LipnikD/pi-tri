22460cbcd672a33cfcb5f8a42ff2e8a861134ff6
Lipnik Dmitry <lipnik-dimm@yandex.ru>
2024-01-20 12:18:05 +0300

    Init

--- src/PiTri.java
+++ src/PiTri.java
@@ -24,6 +24,7 @@ public class PiTri {
                 }
                 a++;
             }
+
         } else if (args.length == 3) { //проверить три числа
             int a = Integer.parseInt(args[0]);
             int b = Integer.parseInt(args[1]);
